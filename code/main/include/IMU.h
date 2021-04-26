d77aef1f9ea10336fb538e724049c0c64af38ba7
Szymon Stanik <szymon.stanik@relayr.io>
2021-04-27 01:30:02 +0200

    refactor

---
+++ code/main/include/IMU.h
@@ -0,0 +1,25 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include "MPU.hpp"
+#include "SPIbus.hpp"
+#include "mpu/math.hpp"
+#include "mpu/types.hpp"
+
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
+
+static constexpr int MOSI = 13;
+static constexpr int MISO = 12;
+static constexpr int SCLK = 9;
+static constexpr int CS = 10;
+static constexpr uint32_t CLOCK_SPEED = 1000000;  // up to 1MHz for all registers, and 20MHz for sensor data registers only
+
+
+void vMPU( void * pvParameters );
+
+#endif // IMU_H
