260ec9473d616859db960715cb5e467dbe3701b2
Szymon Stanik <stanik.szymon@gmail.com>
2022-09-06 17:35:32 +0200

    Implemented Kalman filter in matlab

--- matlab/kalman_filter.m
+++ matlab/kalman_filter.m
@@ -1,9 +1,7 @@
-% SHOW DATA
-T = readtable('./acc_gyro_data.csv');
-data = table2array(T);
+data = csvread('./acc_gyro_data.csv');
 
+% CALCULATE ANGLE FROM GYROSCOPE DATA
 gyro = zeros(1, size(data,1));
-
 gyro(1) = 0;
 gyro_sum = 0;
 for i = 2:size(data,1)
@@ -11,33 +9,63 @@ for i = 2:size(data,1)
     gyro(i) = gyro_sum/1130;
 end
 
-%figure
-%plot(1:size(data,1), data(:,1), 'r')
-%hold on
-%plot(1:size(data,1), gyro, 'b')
-
 % KALMAN FILTER IMPLEMENTATION
-dt = 1;
-x = [0, 0]';  % [omega, theta]
-P = [0, 0;
-     0, 0];
- 
-A = [1, -dt;
+h = 0.05;
+
+x = [0, 0]';
+P = [10, 0;
+     0, 10];
+
+F = [1, -h;
      0, 1];
-H = [1, 0;
-     0, 0];
-Q = [0, 0;
-     0, 0];
-R = [0, 0;
-     0, 0];
+G = [h, (-h*h)/2;
+     0, h];
+H = eye(2);
+
+b = 0.01
+R = b*eye(2);
+Q = eye(2);
  
-% implementation
-for i = 2:size(data,1)
-    x(:,i) = A*x(:,i-1);
-    P(:,:,i) = A*P(:,:,i-1)*A' + Q;
-    K = (P(:,:,i)*H')\(H*P(:,:,i)*H' + R);
-    x(:,i) = x(:,i) + K*H'*(data(i,:)' - H*x(:,i));
-    P(:,:,i) = P(:,:,i) - K*H*P(:,:,i);
+for i = 1:size(data,1)
+    %K = (F*P(:,:,i)*H')/(R + H*P(:,:,i)*H');
+    p11 = P(1,1,i);
+    p12 = P(1,2,i);
+    p21 = P(2,1,i);
+    p22 = P(2,2,i);
+ 
+    detK = 1/(b*b + b*p11 + b*p22 + p11*p22 - p12*p21);
+    K = [ ((p11 - h*p22)*(b + p22) - p21*(p12 - h*p22))*detK,  (-p12*(p11 - h*p21) + (b + p11)*(p12 - h*p22))*detK  ;
+          (p21*(b + p22) - p21*p22)*detK,                      (-p12*p21 + p22*(b + p11))*detK                      ];
+
+  
+    %x(:,i+1)  = F*x(:,i) + K*([data(i,1),gyro(i)]'-H*x(:,i));
+    k11 = K(1,1);
+    k12 = K(1,2);
+    k21 = K(2,1);
+    k22 = K(2,2);
+    
+    x1 = x(1,i);
+    x2 = x(2,i);
+    y1 = data(i,1);
+    y2 = gyro(i);
+    x(:,i+1) = [ x1 - h*x2 + k11*(y1-x1) + k12*(y2-x2) ;
+                 x2 + k21*(y1-x1) + k22*(y2-x2)        ];
+    
+    %P(:,:,i+1) = F*P(:,:,i)*F' + G*Q*G' - K*H*P(:,:,i)*F';
+    aa = p11 - h*p21 - h*(p12-h*p22) + h*h + (h*h*h*h)/4 - (k11*p11 + k12*p21 - h*(k11*p12 + k12*p22));
+    bb = p12 - h*p22 - (h*h*h)/2 - (k11*p12 + k12*p22);
+    cc = p21 - h*p22 - (h*h*h)/2 - (k21*p11 + k22*p21 - h*(k21*p12 + k22*p22));
+    dd = p22 + h*h   - (k21*p12 + k22*p22);
+    
+    P(:,:,i+1) = [aa, bb; cc, dd];    
 end
 
-x(:,500)
+% SHOW DATA
+n = size(data,1)
+figure
+plot(1:n, data(:,1), 'b')
+hold on
+plot(1:n, gyro, 'g')
+hold on
+plot(1:n+1, x(1, :), 'r')
+legend('accel','gyro', 'kalman')
