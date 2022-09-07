2267f3dbf7e3d40d4fec2c9ef2cc6c4c8eb3ed89
Szymon Stanik <stanik.szymon@gmail.com>
2022-09-08 00:34:11 +0200

    Added example UDP client

---
+++ code/main/include/UdpClient.h
@@ -0,0 +1,10 @@
+//
+// Created by szymon on 04.09.22.
+//
+
+#ifndef BOLT_UDP_CLIENT_H
+#define BOLT_UDP_CLIENT_H
+
+void vTaskUdpClient( void * pvParameters );
+
+#endif //BOLT_UDP_CLIENT_H
--- code/main/src/main.cpp
+++ code/main/src/main.cpp
@@ -1,9 +1,11 @@
 #include "motors.h"
 #include "IMU.h"
 #include "control.h"
+#include "UdpClient.h"
 
 extern "C" void app_main() {
     xTaskCreate(vTaskMotors, "task_motors", 1024*4, nullptr, 3, nullptr);
     xTaskCreate(vtaskMPU, "task_mpu", 1024*4, nullptr, 3, nullptr);
     xTaskCreate(vTaskControl, "task_control", 1024*4, nullptr, 3, nullptr);
+    xTaskCreate(vTaskUdpClient, "task_control", 1024*4, nullptr, 3, nullptr);
 }
